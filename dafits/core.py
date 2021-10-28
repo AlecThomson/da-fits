7bf48c5c13bdd62f40bab31c46cb705b2f28f829
Thomson, Alec (S&A, Kensington) <alec.thomson@csiro.au>
2021-10-28 13:00:34 +0800

    Add write methods

--- dafits/core.py
+++ dafits/core.py
@@ -2,63 +2,87 @@
 # -*- coding: utf-8 -*-
 
 import dask.array as da
+import shutil
+from dataclasses import dataclass
 
+@dataclass
 class DaFits:
-    def __init__(self, file: str, ext=0, chunks='auto', use_fitsio=True, memmap=True, mode='denywrite'):
-        """Extract FITS data as Dask 
+    def __init__(self, data = None, header = None, chunks='auto'):
+        """Class for handling FITS files as Dask Arrays.
 
         Args:
-            file (str): Name of FITS file.
-            ext (int, optional): HDU extension to select. Defaults to 0.
-            chunks (int, tuple, optional):
-                How to chunk the array. Must be one of the following forms:
-
-                - A blocksize like 1000.
-                - A blockshape like (1000, 1000).
-                - Explicit sizes of all blocks along all dimensions like
-                ((1000, 1000, 500), (400, 400)).
-                - A size in bytes, like "100 MiB" which will choose a uniform
-                block-like shape
-                - The word "auto" which acts like the above, but uses a configuration
-                value ``array.chunk-size`` for the chunk size
-
-                -1 or None as a blocksize indicate the size of the corresponding
-                dimension.
-            use_fitsio (bool, optional): Use FITSIO for IO. Defaults to True. Otherwise uses astropy.io.
-            memmap (bool, optional): For astropy,fts. Defaults to True.
-            mode (str, optional): For astropy.io.fits. Defaults to 'denywrite'.
-        Attributes:
-            data (dask.array.Array): Dask Array wrapper around data.
-            header (header): FITS header. Read by either astropy or FITSIO.
-        """        
-        self.file = file
+            data (da.Array, optional): Dask Array data. Defaults to None.
+            header (header, optional): FITS header. Defaults to None.
+            chunks (str, optional): Dask Array chuncks. Defaults to 'auto'.
+            verbose (bool, optional): Verbose output. Defaults to False.
+        """               
         self.chunks = chunks
-        self.ext = ext
-        self.memmap = memmap
-        self.mode = mode
-
-        if use_fitsio:
-            data, header = self.read_fitsio()
-        else:
-            data, header = self.read_fits()
-
         self.data = data
         self.header = header
 
-    def read_fits(self):
+    def read_fits(self, file: str, ext=0, memmap=True, mode='denywrite'):
         from astropy.io import fits
-        with fits.open(self.file, memmap=self.memmap, mode=self.mode) as hdul:
-            hdu = hdul[self.ext]
+        with fits.open(file, memmap=memmap, mode=mode) as hdul:
+            hdu = hdul[ext]
             data = hdu.data
             header = hdu.header
         array = da.from_array(data, chunks=self.chunks)
-        return array, header
+        self.data = array
+        self.header = header
 
-    def read_fitsio(self):
-        from fitsio import FITS,FITSHDR
-        with FITS(self.file) as hdul:
-            hdu = hdul[self.ext]
+    def read_fitsio(self, file: str, ext=0):
+        from fitsio import FITS
+        with FITS(file) as hdul:
+            hdu = hdul[ext]
             data = hdu.read()
             header = hdu.read_header()
         array = da.from_array(data, chunks=self.chunks)
-        return array, header
+        self.data = array
+        self.header = header
+
+    def write_fits(self, file, header=None, verbose=True, **kwargs):
+        """Write Dask Array to FITS file.
+
+        Args:
+            file (str): Name of FITS file.
+            header (header, optional): FITS header. Defaults to None.
+            **kwargs:
+        """
+        from astropy.io import fits
+        if header is None:
+            header = self.header
+        tmp_file, z_data = self.write_tmp_zarr(file, self.data, self.verbose)
+        hdu = fits.PrimaryHDU(z_data, header=header)
+        hdu.writeto(file, **kwargs)
+        if self.verbose:
+            print(f'Wrote FITS file: {file}')
+        shutil.rmtree(tmp_file)
+
+    def write_fitsio(self, file, header=None, verbose=True, **kwargs):
+        """Write Dask Array to FITS file.
+
+        Args:
+            file (str): Name of FITS file.
+            header (header, optional): FITS header. Defaults to None.
+            **kwargs:
+        """
+        from fitsio import FITS
+        if header is None:
+            header = self.header
+        tmp_file, z_data = self.write_tmp_zarr(file, self.data, self.verbose)
+        hdu = FITS(file, mode='rw')
+        hdu.write(z_data, header=header, **kwargs)
+        hdu.close()
+        if self.verbose:
+            print(f'Wrote FITS file: {file}')
+        shutil.rmtree(tmp_file)
+
+    @staticmethod
+    def write_tmp_zarr(file, data, verbose=False):
+        import zarr
+        tmp_file = file.replace('.fits', '_tmp.zarr')
+        if verbose:
+            print(f'Writing temporary zarr file: {tmp_file}')
+        data.to_zarr(tmp_file)
+        z_data = zarr.open(tmp_file, mode='r')
+        return tmp_file, z_data
